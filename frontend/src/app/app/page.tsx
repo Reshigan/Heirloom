d86e2ec6351703e7884ca21c91d9c82b2c6201ae
reshigan@gonxt.tech <reshigan@gonxt.tech>
2025-11-21 13:31:14 +0000

    Add marketing landing page at root, move app to /app route, add viewport metadata and mobile touch handlers

---
+++ frontend/src/app/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import FuturisticHeirloomInterface from '@/components/futuristic-heirloom-interface';
+
+export default function Home() {
+  return (
+    <div className="relative">
+      <FuturisticHeirloomInterface />
+    </div>
+  );
+}
